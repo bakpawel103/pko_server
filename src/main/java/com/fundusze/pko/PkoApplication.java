d817018210fa70ba86bb560edef96555589690e5
Pawel Bak <215675@edu.p.lodz.pl>
2020-10-29 19:59:45 +0100

    Initializing project

---
+++ src/main/java/com/fundusze/pko/PkoApplication.java
@@ -0,0 +1,13 @@
+package com.fundusze.pko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundusze/pko/PkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundusze.pko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
